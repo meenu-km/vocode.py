37ac18b51eb71316764f02ddd551329c01a3d619
Neha Ajith <nehaajith27@gmail.com>
2023-04-06 14:55:12 +0530

    api function updated

--- test/main.py
+++ test/main.py
@@ -1,25 +1,45 @@
 from fastapi import FastAPI
-import torch
-from transformers import AutoTokenizer
+import openai
 
 app = FastAPI()
 
-model = torch.load('model.pt')
-tokenizer = AutoTokenizer.from_pretrained("tokenizer")
+MAX_TOKEN_DEFAULT = 128
+TEMPERATURE = 0.0
+
+STREAM = True
+
+def initialize_openai_api():
+    openai.api_key = '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'
+
+def create_input_prompt(englishTextIn=""):
+    prompt = "The following bot transforms natural language to Python code. \n" + \
+             "Input: {}\n".format(englishTextIn) + \
+             "Python: \n '''python"
+    return prompt
+
+
+def generate_completion(input_prompt, num_tokens=MAX_TOKEN_DEFAULT):
+    stop_string = "'''\n"
+    response = openai.Completion.create(engine='text-davinci-002', prompt=input_prompt, temperature=TEMPERATURE,
+                                        max_tokens=num_tokens, stream=STREAM, stop=stop_string,
+                                        top_p=1.0, frequency_penalty=0.0, presence_penalty=0.0)
+    return response
+
+
+def get_generated_response(response):
+    generatedCode = "## Python code generated from plain english: \n"
+    while True:
+        nextResponse = next(response)
+        completion = nextResponse['choices'][0]['text']
+        generatedCode = generatedCode + completion
+        if nextResponse['choices'][0]['finish_reason'] is not None:
+            break
+    return generatedCode
 
 @app.get("/")
-def predict():
-    prompt = '''def binarySearch(arr, left, right, x):
-    mid = (left +'''
-    input_ids = tokenizer.encode(prompt, return_tensors='pt')
-    result = model.generate(input_ids, max_length=100, num_beams=1, num_return_sequences=1)
-    op = []
-    for res in result:
-        op.append(tokenizer.decode(res))
+def predict(englishText:str):
+    prompt = create_input_prompt(englishText)
+    response = generate_completion(prompt)
+    op = get_generated_response(response)
     
     return{"output":op}
-
-
-# @app.get("/get-student/{student_id}")
-# def get_student(student_id: int = (None,description="The ID of the student you want to view")):
-#     return students[student_id]
